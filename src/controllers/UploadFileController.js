34e112a8c7bbcff1bb23f34e43871a94b0069d7b
Iaiky <Iaikitiananaej@gmail.com>
2023-10-19 22:18:49 +0300

    update inscription and delete commande

--- src/controllers/UploadFileController.js
+++ src/controllers/UploadFileController.js
@@ -4,8 +4,8 @@ var db = require("../configs/connect")
 module.exports = {
     // upload file  
     upload : (req, res, next) => {
-        res.json({"message": "success",
-        file:req.file})
+        console.log(req.file);
+        res.json({ message: 'success', file: req.file })
         // res.send("upload successfull")
     },
 
